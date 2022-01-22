f2461f9c8964fe47eda15ef4aed690d0229ed92a
Johan Song <70825076+toxicbeans@users.noreply.github.com>
2022-01-22 00:23:45 -0500

    Johandev (#2)
    * bad code
    
    * fix textarea error
    
    Co-authored-by: Johan Song <Johan@Johans-MacBook-Air.local>

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import Nameform from './input.js';
 import Prompt from "./main-game/Word.js"
 import NavigationBar from './navbar/NavigationBar';
 import AboutUs from './about-us/AboutUs';
@@ -10,6 +11,7 @@ function App() {
       <Prompt content="hello" speed={10}/>
       <NavigationBar></NavigationBar>
       <AboutUs></AboutUs>
+      <Nameform></Nameform>
     </div>
   );
 }

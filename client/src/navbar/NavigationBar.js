0d6469e00d6e024003d0347df37e54260975d007
DivSam <48162524+DivSam@users.noreply.github.com>
2022-01-22 00:01:08 -0500

    adding router imports

--- client/src/navbar/NavigationBar.js
+++ client/src/navbar/NavigationBar.js
@@ -1,36 +1,30 @@
 import React, { Component } from "react";
 import { MenuItems } from "./MenuItems";
 import "./NavigationBar.css";
-
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 class NavigationBar extends Component {
   state = { clicked: false };
 
   handleClck = () => {
-    this.setState({ clicked: !this.state.clicked });
+    window.open("../hidden.js");
   };
 
   render() {
     return (
-      <nav className="NavbarItems">
-        <h1 className="Title">THE TYPING GAME</h1>
-        
-        <ul className={this.state.clicked ? "nav-menu active" : "nav-menu"}>
-          {MenuItems.map((item, index) => {
-            return (
-              <li key={index}>
-                <a className={item.cName} href="item.url">
-                  {item.title}
-                </a>
-              </li>
-            );
-          })}
-        </ul>
-      </nav>
+      <Router>
+        <nav className="NavbarItems">
+          <h1 className="Title">THE TYPING GAME</h1>
+          <ul>
+            <Link to="/hidden">hidden</Link>
+          </ul>
+        </nav>
+      </Router>
     );
   }
 }
 
-
-
 export default NavigationBar;
+function hidden() {
+  return <h2>Home</h2>;
+}

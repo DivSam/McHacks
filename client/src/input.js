6d10b1d55a53ee28593c76a0cd71caad9206f59f
Johan Song <Johan@Johans-MacBook-Air.local>
2022-01-21 23:14:28 -0500

    bad code

---
+++ client/src/input.js
@@ -0,0 +1,17 @@
+import React, { Component, createContext } from 'react';
+
+
+export default class myInput extends Component{
+    constructor(props) {
+        super(props)
+        this.state = {
+            text:'current text'
+        };
+    }
+    render() {
+        return (
+            <div>potato</div>
+        );
+
+    }
+}

7556870af58d88cb11140b489634005f8a797edd
DivSam <48162524+DivSam@users.noreply.github.com>
2022-01-22 16:46:24 -0500

    dunno man

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,13 @@
-
-// function App() {
-//   return (
-//     <div className="App">
-//       <NavigationBar></NavigationBar>
-//       <AboutUs></AboutUs>
-//       <PromptController initWords={["hello", "goodbye", "potato"]}/>
-
-import logo from './logo.svg';
-import PromptController from './main-game/PromptController';
-import NavigationBar from './navbar/NavigationBar';
+import logo from "./logo.svg";
+import PromptController from "./main-game/PromptController";
 import "./App.css";
-import Prompt from "./main-game/Word.js";
+import Prompt from "./main-game/Prompt.js";
 import AboutUs from "./about-us/AboutUs";
 import Home from "./home-page/Home";
 import { Routes, Route, Link } from "react-router-dom";
 import NameForm from "./main-game/input";
+import TypingGameComponent from "./second-game/hidden";
+
 function App() {
   return (
     <div className="App">
@@ -23,9 +16,16 @@ function App() {
         <Route path="about" element={<AboutUs></AboutUs>}></Route>
         <Route
           path="prompt"
-          element={<Prompt content="hello" speed={10}></Prompt>}
+          element={
+            <PromptController initWords={["hello", "goodbye", "potato"]} />
+          }
         ></Route>
         <Route path="text" element={<NameForm></NameForm>}></Route>
+
+        <Route
+          path="hidden"
+          element={<TypingGameComponent></TypingGameComponent>}
+        ></Route>
       </Routes>
     </div>
   );
